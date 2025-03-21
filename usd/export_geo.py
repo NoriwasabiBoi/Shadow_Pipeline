d43f3704905569e35b20768412c1ad707c962b85
PhongShaderFrog <robmuc@gmx.de>
2025-03-21 13:00:16 +0100

    functions return xform paths

--- usd/export_geo.py
+++ usd/export_geo.py
@@ -1,18 +1,32 @@
 from pxr import Usd, UsdGeom
 
-def write_usd_file(filepath, ):
+def write_usd_file(filepath, asset_name):
     """
 
     :param filepath:
     :return:
     """
-    stage = Usd.Stage.createNew(filepath)
+    stage = Usd.Stage.CreateNew(filepath)
+    xformPrim = UsdGeom.Xform.Define(stage, f'/{asset_name}')
+    return stage
 
+def create_proxy_layer(stage, asset_name):
+    """
+    :return:
+    """
+    proxy = UsdGeom.Xform.Define(stage, f'/{asset_name}/proxy')
+    #proxy_mesh = UsdGeom.Mesh.
+    return proxy.GetPath()
 
-def create_proxy():
+def create_render_layer(stage, asset_name):
     """
 
     :return:
     """
+    render = UsdGeom.Xform.Define(stage, f'/{asset_name}/render')
+    return render.GetPath()
 
-def create_render():
+stage = write_usd_file("L:/rauchertofu/01_assets/pipeline/test.usda", "capybara")
+create_proxy_layer(stage=stage, asset_name="capybara")
+create_render_layer(stage=stage, asset_name="capybara")
+stage.GetRootLayer().Save()
