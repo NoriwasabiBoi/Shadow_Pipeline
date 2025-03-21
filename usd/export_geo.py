0d2cb6752e851910c442f3169ca188babbfb5749
PhongShaderFrog <robmuc@gmx.de>
2025-03-21 10:43:30 +0100

    created export usd.py

---
+++ usd/export_geo.py
@@ -0,0 +1,18 @@
+from pxr import Usd, UsdGeom
+
+def write_usd_file(filepath, ):
+    """
+
+    :param filepath:
+    :return:
+    """
+    stage = Usd.Stage.createNew(filepath)
+
+
+def create_proxy():
+    """
+
+    :return:
+    """
+
+def create_render():
